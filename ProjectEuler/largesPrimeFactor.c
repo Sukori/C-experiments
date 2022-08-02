c93175bc17efb17c117241556c91d2a4f2ed2b3a
PBerset <sukori98@gmail.com>
2022-08-02 17:08:59 +0200

    ajout du problème 3

---
+++ ProjectEuler/largesPrimeFactor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+Probleme 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+Bruteforce: en comptant depuis 1, trouver des nombres premiers et diviser le nombre par chacun d'eux en stockant la valeur du plus grand qui a fonctionné.
+C'est stupide et ça peut faire des tonnes d'itérations.
+
+Déjà, si on cherche le plus grand diviseur, on peut rester au dessous de la racine carrée du nombre, parce qu'on ne peut pas avoir un nombre premier plus grand que ça comme facteur.
+
+Merci Wolfram alpha: 6857 est le nombre qu'on recherche.
+Prime factors: 71×839×1471×6857 = 600851475143
+*/
+
+int main(){
+
+
+    return 0;
+}
