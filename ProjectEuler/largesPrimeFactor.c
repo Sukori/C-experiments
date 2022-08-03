3fe8d227c07c8187617ff32551290ed7dedda512
PBerset <sukori98@gmail.com>
2022-08-03 17:27:36 +0200

    problème 3 de Project Euler

--- ProjectEuler/largesPrimeFactor.c
+++ ProjectEuler/largesPrimeFactor.c
@@ -12,12 +12,63 @@ C'est stupide et ça peut faire des tonnes d'itérations.
 
 Déjà, si on cherche le plus grand diviseur, on peut rester au dessous de la racine carrée du nombre, parce qu'on ne peut pas avoir un nombre premier plus grand que ça comme facteur.
 
+Un nombre premier n'est jamais pair, donc on peut ignorer tous les nombres pairs dans nos calculs.
+On peut aussi dégager les multiples de 5 et de 3 et commencer à partir de 7.
+Les multiples de 5 et 3 sont obvious et très nombreux. les multiples de 7, déjà moins.
+Donc on peut compter de 2 en 2 à partir de 7 pour ne pas inutilement tester les nombres pairs.
+
 Merci Wolfram alpha: 6857 est le nombre qu'on recherche.
 Prime factors: 71×839×1471×6857 = 600851475143
+
+On commence à 7
+On teste si le nombre est divisible par 3-5, si oui, on skip
+sinon on essaie de diviser N par notre nombre.
+Si divisible, on place le nombre en buffer jusqu'à en trouver un plus grand
+sinon, on recommence avec le n+2 (donc 9 après 7, comme on évite les nombres pairs).
+
+Le buffer est une liste, parce qu'on veut aussi vérifier que le nombre trouvé, n'est pas aussi un multiple d'un nombre premier déjà trouvé, non ?
+
+    //function test prime(n)
+    //function test divide (n,N) -> function test prime (n+2)
 */
+
+static int theNumber = 600851475143;
+static int i = 7;
+int testPrime(int);
+int testDivide(int);
 
 int main(){
 
+    int result = testDivide(7);
+
+    printf("Le plus grand facteur premier de %i est : %i", theNumber, result);
 
     return 0;
+}
+
+int testPrime(n){
+    if(n % 3 == 0 || n % 5 == 0){
+        return false;
+    }
+
+    if(n == i){
+        return true;
+    }
+
+    if(n % i == 0){
+        return false;
+    }
+
+    i += 2;
+    return testPrime(n);
+}
+
+int testDivide(n){
+    if(!testPrime(n) || theNumber % n != 0){
+        if(n < sqrt(theNumber)){
+            return testDivide(n+2);
+        }
+    }
+
+    return n;
 }
