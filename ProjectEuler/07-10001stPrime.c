d3147535d32cbe4f2860fea13880e0fcd889eebf
PBerset <sukori98@gmail.com>
2022-08-24 17:41:06 +0200

    06: rename 07: still searching better than bruteforcing

--- ProjectEuler/07-10001stPrime.c
+++ ProjectEuler/07-10001stPrime.c
@@ -6,46 +6,50 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10'001st prime number?
 
 
-Donc j'avais fait un truc du genre de rechercher les nombres premiers à partir de 21 (ou 23) et utiliser tous les nombres premiers trouvés pour les ajouter à la liste des nombres dont les multiples sont à ignorer, pour ignorer de plus en plus de nombre et ne pas faire péter le processeur.
-On ignore aussi tous les nombres paires.
+On ignore tous les nombres paires.
+La bruteforce s'appelle le tamis d'Eratosthène, mais à ma sauce, parce que là je me fiche de compter 2, 3, 5,... Je commence directement plus haut pour sauter quelques pas.
+Aussi je peux ignorer les nombres pairs dans la boucle, plutôt que de tester %2.
+J'avais vu aussi dans 03 qu'on pouvait se contenter de compter jusqu'à sqrt du nombre, plutôt que tout faire
+
+alors on veut faire:
+
+on commence avec nombre en cours
+on regarde si le nombre en cours est premier
+    on le compare % i, ou 3 < i < sqrt(nombre en cours) ; i+2 osef des nombres paires
+    si on a % = 0 alors pas premier, continue
+    si on sort de la boucle sans % = 0
+        Result = le nombre en cours, qui est premier
+        position en cours ++
+        et on retente la boucle avec le nombre + 2
+si position en cours == position max, alors result == le 10001e nombre premier
 */
 
-int premiers[];
+int positionEnCours;
 
-int multiplePremier(int, int[]);
-
-int positionAtrouver;
+int positionMax;
 
 int nombreEnCours;
 
-int limiteur;
+int result; //le plus grand premier trouvé à ce moment
 
+int main(){
 
-int main()
-{
-
-    printf("Quel nombre premier veux tu trouver ?\n");
-    scanf("%i\n", &positionAtrouver);
-
-    //Doom, mettre un limiteur
-    while(length(premiers) < positionAtrouver || limiteur == 0){
-        limiteur--;
-        int step = multiplePremier(nombreEnCours, premiers);
+    //Doom
+    while(positionEnCours < positionMax){
+        
+    
     }
+
+    printf("Le 10001-ième nombre premier est %d.\n", result);
     
     return 0;
 }
 
-int limiteur = 100;
+int nombreEnCours = 3; // on sait qu'après 29, c'est 31 le nombre premier suivant.
 
-int premiers[] = {3,5,7,11,13,17,19};
+int positionEnCours = 10; // parce qu'il y a dix nombres premiers de 2 à 29 inclus.
 
-int nombreEnCours = 23;
+int positionMax = 10001;
 
-int multiplePremier(int nombre, int premiersTrouve[]){
-    for (int i = 0; i < length(premiersTrouve); i++)
-    {
-        /* code */
-    }
-    
-}
+int result
+
