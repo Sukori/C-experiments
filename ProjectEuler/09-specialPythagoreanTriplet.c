7aefeecb2f7010718b6eb65baa18d2ed9446ce01
PBerset <sukori98@gmail.com>
2022-08-26 21:46:37 +0200

    09 résolu à la main et en code bruteforce. TODO: reecrire la solution à la main en code en adaptant le problème 03

--- ProjectEuler/09-specialPythagoreanTriplet.c
+++ ProjectEuler/09-specialPythagoreanTriplet.c
@@ -25,9 +25,50 @@ Il n'y a exactement qu'une seule solution.
 J'ai la preuve que trouver =1000 et < ne suffit pas. Donc le début c'est plutôt de trouver les ^2 qui vérifient l'égalité.
 
 Brute force : tout tester un par un jusqu'à 1 + 2 + 997 puis recommencer en incrémentant b XD. Dans 1000 ans, on y est encore.
-Note 0 + 499 + 501 = 1000. a = 0 est autorisé XD, mais pas forcément utile ... Surtout qu'on cherche le résultat du produit des 3 nombres, ça serait triste d'avoir 0 XD.
+Note 0 + 499 + 501 = 1000. a = 0 est autorisé XD, mais pas forcément utile ... Surtout qu'on cherche le résultat du produit des 3 nombres, ça serait triste d'avoir 0 XD. le minimum requis c'est 0<1<2 pour a<b<c
 
-Bon, sinon, c'est une occasion de faire de la trigo avec sinus/cosinus.
+allez, pour la doc:
+for(int c = 2; c < 1000; c++)
+    for(int b = 1; b < c; b++)
+        for(int a = 0; a < b; a++)
+            if(a+b+c == 1000)
+                return (a*b*c)
+
+Voila, c'est tout con et ça ne prend que quelques lignes pour un calcul bien long.
+
+maintenant Si on veut résoudre d'une manière plus élaborée, il y a la formule d'Euclide. Mais elle sort d'un bouquin. J'ai envie de calculer moi-meme.
+
+par exemple, en factorisant un tit peu, on peut isoler c = 1000 - a - b
+
+donc c^2 = (1000 - a - b)^2
+a^2 + b^2 = 1'000'000 + a^2 + b^2 + 2ab - 2000a - 2000b
+On constate avec l'égalité ci-dessus que 1'000'000 + 2ab -2000a - 2000b = 0
+Continuons: ab - 1000a - 1000b = -500'000
+on veut inverser le signe du -500'000, donc on peut faire  + 1'000'000
+
+1'000'000 + ab -1000a - 1000b = 500'000
+Et on peut factoriser::
+(1000 - a) (1000 - b) = 500'000
+
+De là, on peut essayer de trouver les valeurs de a et b en cherchant des facteurs premiers de 500'000 : 2^5 * 5^6. nommons les F1 et F2 -> exercice prime factors.
+On peut supposer 700^2 = 490'000, qui est assez proche de 500'000, donc on recherche des nombres proches de 700 pour a et b.
+Prenons large 500 < F1,F2 < 1000
+F1, 2^5 = 32, on n'est pas dans l'intervale 500 - 1000. En plus, il n'a pas de facteur 5 dans 32, donc on peut retirer 5 pour les deux facteurs:
+2^5 * 5^4 : 100 < F1/5, F2/5 < 200, 32 n'est toujours pas dans l'intervalle, alors on retire encore deux facteurs 5
+2^5 * 5^2 : 20 < F1/5^2, F2/5^2 < 40, là on arrive à fit 32, et l'autre facteur est 5^2 = 25
+32 * 32 = 1024, on est plus haut que 1000
+32 * 25 = 800, parfait
+25 * 25 = 625, parfait aussi
+
+on peut donc poser:
+F1 = 1000 - a = 800 -> a = 200
+F2 = 1000 - b = 625 -> b = 375
+
+donc c = 1000 - 200 - 375 = 425
+
+et voilà
+
+Ma fonction prime factors du problème 03 doit etre réécrite parce que j'ai gratté pour résoudre l'autre problème avec moins d'étapes. En plus, là on cherche sur un nombr pair, pas impair.
 */
 
 int main(){
